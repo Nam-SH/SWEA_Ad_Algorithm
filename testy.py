fe740983770b37d7ccf5c0dee3fe88e2d6815fdf
Nam-SH <gtsmell@gmail.com>
2019-09-03 23:42:03 +0900

    20190903 | Update A. Preparation

--- testy.py
+++ testy.py
@@ -1,16 +1,42 @@
-import sys; sys.stdin = open('data/(5189)input.txt', 'r')
-from itertools import permutations
+import sys; sys.stdin = open('data/(A1)input.txt', 'r')
 
-for tc in range(int(input())):
+def find(sfdata):
+    global res, cnt, lst
+
+    if cnt > 5: return
+
+    if len(lst) == N:
+        if lst == sorted(lst) or lst == sorted(lst, reverse=True):
+            res = cnt
+            return
+
+
+
+
+def shuffle(x, l, r):
+    global lst
+
+    if x >= 3:
+        x = 5 - x
+        l, r = r, l
+    while l and r:
+        for _ in range(3 - x):
+            if l: lst.append(l.pop(0))
+        for _ in range(3 - x):
+            if r: lst.append(r.pop(0))
+    return lst
+
+t = int(input())
+for tc in range(2):
     N = int(input())
-    arr = [list(map(int, input().split())) for _ in range(N)]
-
-    Min = 99999
-    for lst in permutations(range(1, N)):
-        sub = arr[0][lst[0]]
-        for i in range(len(lst) - 1):
-            sub += arr[lst[i]][lst[i + 1]]
-        sub += arr[lst[i + 1]][0]
-        if Min > sub:
-            Min = sub
-    print('#{} {}'.format(tc + 1, Min))
+    arr = list(map(int, input().split()))
+    lst = []
+    cnt = 0
+    res = -1
+    for x in range(6:)
+        find(shuffle(x, arr[:N//2], arr[N//2:]))
+        if 0 <= res <=5:
+            print(res)
+            break
+    else:
+        print(-1)
