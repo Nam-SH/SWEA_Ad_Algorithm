8a69b23cb3dd3203c9ac05b4293a74c61b4910e6
Nam-SH <gtsmell@gmail.com>
2019-09-11 22:53:38 +0900

    20190911 | Update code

--- testy.py
+++ testy.py
@@ -1,3 +1,12 @@
-import sys; sys.stdin = open('data/(2383)input.txt', 'r')
+import sys; sys.stdin = open('data/(5185)input.txt', 'r')
 
+def find(N):
+    res = ''
+    for i in range(int(N)):
+        if len(data[i]) < 4: res += format(int(data[i], 16), 'b').zfill(4)
+        else: res += format(int(data[i], 16), 'b')
+    return res
 
+for tc in range(int(input())):
+    N, data = input().split()
+    print('#{} {}'.format(tc + 1, find(N)))
