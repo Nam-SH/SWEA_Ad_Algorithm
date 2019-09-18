0824057dacf139eb1601f3fc3e768bcd40acbd48
Nam-SH <gtsmell@gmail.com>
2019-09-18 23:49:05 +0900

    20190918 | Update

--- testy.py
+++ testy.py
@@ -1,61 +1,20 @@
-import sys; sys.stdin = open('data/(5247)input.txt', 'r')
+import sys; sys.stdin = open('data/(5248)input.txt', 'r')
 
+T = int(input())
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
 
-from collections import deque
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
 
-def BFS():
-    global start_num, end_num, result, tc
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
 
-    q = deque()
-    q.append((start_num, 0))
+check = [[] for _ in range(N + 1)]
 
-    while q:
-        num, cnt = q.popleft()
-        if num == end_num:
-            result = cnt
-            return
+for i in range(0, len(arr) - 1, 2):
+    check[arr[i]].append(arr[i + 1])
 
-        for i in range(4):
-            if not (0 < eval(N + operation[i]) <= 1000000): continue
-            # if num_lst[num2] != tc:
-            q.append((eval(N + operation[i]), cnt+1))
+print(check[1:])
 
-TC = int(input())
-for tc in range(1, TC+1):
-    N, M = map(int, input().split())
-    num_lst = [0] * 1000001
-    num_lst[N] = tc
-    result = 0
-    BFS()
-    print('#%d %d'%(tc, result))
-
-
-
-def find(N):
-    global Min, cnt
-    print(N)
-    if cnt >= Min: return
-
-    if int(N) == int(M):
-        Min = min(Min, cnt)
-        return
-
-    for i in range(4):
-        if not (0 < eval(N + operation[i]) <= 1000000): continue
-        if check[i]: continue
-        check[i] = 1
-        cnt += 1
-        find(str(eval(N + operation[i])))
-        cnt -= 1
-        check[i] = 0
-
-
-int(input())
-for tc in range(2):
-    N, M = input().split()
-    Min = 9999
-    check = [0]*4
-    operation = ['+1', '-1', '*2', '-10']
-    cnt = 0
-    find(N)
-    print('#{} {}'.format(tc + 1, Min))
+for i in range(len(check)):
