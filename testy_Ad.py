ef9677a25341f8edf20d530503905b9c50fe5d01
Nam-SH <gtsmell@gmail.com>
2019-09-29 19:57:55 +0900

    20190929 | Update

--- testy_Ad.py
+++ testy_Ad.py
@@ -1,30 +1,32 @@
 import sys; sys.stdin = open('data/(5250)input.txt', 'r')
 
-def DFS(i, j, res):
+from collections import deque
+
+def BFS(y, x, calc):
     global Min
 
-    if res >= Min: return
+    q = deque()
+    q.append((y, x, calc))
 
-    if (i, j) == (N - 1, N - 1):
-        if Min > res:
-            Min = res
-        return
+    while q:
+        i, j, res = q.popleft()
 
-    for dy, dx in (1, 0), (-1, 0), (0, 1), (0, -1):
-        ty = i + dy; tx = j + dx
-        if not (0 <= ty < N and 0 <= tx < N): continue
-        if check[ty][tx]: continue
-        check[ty][tx] = 1
+        if res >= Min: return
 
-        if arr[ty][tx] > arr[i][j]:
-            DFS(ty, tx, res + 1 + arr[ty][tx] - arr[i][j])
-        else:
-            DFS(ty, tx, res + 1)
+        if (i, j) == (N - 1, N - 1):
+            Min = min(Min, res)
+            return
+
+        for dy, dx in (1, 0), (0, 1):
+            ty = i + dy; tx = j + dx
+            if not (0 <= ty < N and 0 <= tx < N): continue
+            if arr[ty][tx] > arr[i][j]: q.append((ty, tx, res + 1 + arr[ty][tx] - arr[i][j]))
+            else: q.append((ty, tx, res + 1))
 
 for tc in range(int(input())):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
     check = [[0] * N for _ in range(N)]
     Min = 99999
-    DFS(0, 0, 0)
+    BFS(0, 0, 0)
     print('#{} {}'.format(tc + 1, Min))
